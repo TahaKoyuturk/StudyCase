2275702c2ade8637128ce2b8f22a20eb55f5dd6b
Thacooo <Thacooo@users.noreply.github.com>
2022-09-25 01:24:01 +0300

    Update PlayerController.cs

--- StudyCase/Assets/Scripts/PlayerController.cs
+++ StudyCase/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody),typeof(BoxCollider))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private FixedJoystick joystick;
+    //[SerializeField] private Animator animator;
+    [SerializeField] private float moveSpeed;
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
+
+    }
     void Start()
     {
         
